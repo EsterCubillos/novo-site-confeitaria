4ffda7c5ec41cfd8a3b6257f890818828b3aed40
Ester Cubillos Matos <espamalu04@gmail.com>
2023-07-19 18:20:43 +0000

    adicionar

---
+++ assets/components/logo.js
@@ -0,0 +1,25 @@
+import { LitElement, css } from "lit";
+
+export class Logo extends LitElement {
+  static styles = [
+    css`
+      :host {
+        width: 48px;
+        height: 48px;
+
+        background-color: black;
+
+        -webkit-mask: url('logo.png') no-repeat center;
+        mask: url('logo.png') no-repeat center;
+        
+
+        -webkit-mask-size: 100%;
+        mask-size: 100%;
+
+        
+        
+      }
+    `,
+  ];
+}
+customElements.define("app-logo", Logo);
